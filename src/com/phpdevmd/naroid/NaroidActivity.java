a3469d201029eb4afd6385250830c9e06fc8e482
Roman V <phpdevmd@gmail.com>
2011-08-29 21:34:25 +0300

    Add sample menu (with quit option)

--- src/com/phpdevmd/naroid/NaroidActivity.java
+++ src/com/phpdevmd/naroid/NaroidActivity.java
@@ -2,6 +2,9 @@ package com.phpdevmd.naroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class NaroidActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,28 @@ public class NaroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    /** Called when the user touches the Menu button.
+     * Options menu is based on R/menu/main.xml.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /** Called when a menu option is pressed. */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.quit:
+        	NaroidActivity.this.finish();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
