046207803761092f898d377d850ccb52cbbb0eea
Roman <phpdevmd@gmail.com>
2011-08-29 15:57:52 +0300

    First commit. Hello World app

---
+++ src/com/phpdevmd/naroid/NaroidActivity.java
@@ -0,0 +1,13 @@
+package com.phpdevmd.naroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NaroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
