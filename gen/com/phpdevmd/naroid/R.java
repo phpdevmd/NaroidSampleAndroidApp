7c7fdfb46e4b251719549f40fe1bf3b63a5f3b71
Roman V <phpdevmd@gmail.com>
2011-08-31 09:36:32 +0300

    introduce simple list view

--- gen/com/phpdevmd/naroid/R.java
+++ gen/com/phpdevmd/naroid/R.java
@@ -17,13 +17,14 @@ public final class R {
         public static final int quit=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int hello_world=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int hello_world=0x7f040000;
     }
 }
