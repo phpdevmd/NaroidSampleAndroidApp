41dd91e86bb38c06b7ea0274b94fc04dde9e639e
Roman V <phpdevmd@gmail.com>
2011-09-02 23:29:11 +0300

    Add simple navigation and main page

--- gen/com/phpdevmd/naroid/R.java
+++ gen/com/phpdevmd/naroid/R.java
@@ -8,23 +8,27 @@
 package com.phpdevmd.naroid;
 
 public final class R {
+    public static final class array {
+        public static final int main_list_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int quit=0x7f060000;
+        public static final int quit=0x7f070000;
     }
     public static final class layout {
         public static final int hello_world=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f050000;
+        public static final int main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int app_name_basic=0x7f040002;
         public static final int hello_world=0x7f040000;
     }
 }
